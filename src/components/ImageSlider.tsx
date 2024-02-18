2dee80a7d24faba2e1ae0dcae298713eebba6f99
mamun-al-hasan <1905023@ugrad.cse.buet.ac.bd>
2024-02-18 13:28:27 +0600

    slider completed

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
-import { Swiper,SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import 'swiper/css/effect-coverflow';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import Image1 from "./../../public/slider/image1.jpg";
 import Image2 from "./../../public/slider/image2.jpg";
@@ -16,40 +13,32 @@ import Image3 from "./../../public/slider/image3.jpg";
 import Image4 from "./../../public/slider/image4.jpg";
 import Image5 from "./../../public/slider/image5.jpg";
 
-const images = [
-    Image1,
-    Image2,
-    Image3,
-    Image4,
-    Image5,
-];
+const images = [Image1, Image2, Image3, Image4, Image5];
 
 const ImageSlider: React.FC = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
-    }, 3000); // Change slide every 3 seconds
-
-    return () => clearInterval(interval);
-  }, []);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true,
+    centerMode: true,
+    centerPadding: "20%",
+  };
 
   return (
-    <Swiper
-      className="mySwiper"
-      spaceBetween={50}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      autoplay={{ delay: 3000 }}
-    >
+    <div className="slick-container">
+      <Slider {...settings}>
         {images.map((image, index) => (
-        <SwiperSlide key={index}>
-          <Image src={image} alt={`Slide ${index}`} />
-        </SwiperSlide>
-      ))}
-      </Swiper>
+          <div key={index} className="slick-slide">
+            <Image src={image} alt={`Slide ${index}`} className="slick-image" />
+          </div>
+        ))}
+      </Slider>
+    </div>
   );
 };
 
